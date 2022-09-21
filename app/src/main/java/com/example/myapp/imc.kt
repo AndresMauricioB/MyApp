1a63bd999054aa903a0380cd734d1864c0856501
AndresMauricioB <andresmauricio5736@gmail.com>
2022-09-20 19:49:35 -0500

    prueba 1 commit

---
+++ app/src/main/java/com/example/myapp/imc.kt
@@ -0,0 +1,22 @@
+package com.example.myapp
+
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.myapp.databinding.ActivityImcBinding
+import java.util.*
+
+class imc : AppCompatActivity() {
+
+    lateinit var bindinig: ActivityImcBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        //  var txtPeso : TextView = findViewById(R.id.pesoImc)
+    }
+
+}
